429e9aa22962855ab3ebf7ba525141d21bf879b1
YoYoBoBo <46692722+YoYoBoBo@users.noreply.github.com>
2020-08-14 14:23:54 -0400

    connect js, some headers

---
+++ public/js/connect.js
@@ -0,0 +1,17 @@
+
+
+function 
+
+function createCollectionItem(){
+    let item = document.createElement("li").classList.add("collection-item").add("avatar");
+    let avatar = document.createElement("img").classList.add("profile-pic");
+    let title = document.createElement("span").classList.add("title");
+    let text = document.createElement("p");
+
+    let info = document.createElement("a").classList.add("secondary-content");
+    let message = document.createElement("a").classList.add("secondary-content");
+    info.href = "#!";
+    message.href = "#!";
+    
+
+}
--- public/js/queries.js
+++ public/js/queries.js
@@ -8,20 +8,22 @@ const firebaseConfig = {
     appId: "1:1025410002463:web:40cec81a8b2a7c8f7caf2a",
     measurementId: "G-Y35S3D1RJ4"
   };
-//alert("yuh");
+alert("yuh");
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-analytics = firebase.analytics();
+
+
+//analytics = firebase.analytics();
 firestore = firebase.firestore();
 
-//alert("na");
+alert("na");
 /**
  * @param {string} userToken - identifier of user info wanted
  */
 
 function getUserInfo(){
-    //alert("hi");
+    alert(firestore);
     let name = firestore.collection("users").where("name", "==", "John Steinbeck").get("field");
-    //alert("nah");
-    //alert(name);
+    alert("nah");
+    alert(name);
 }
