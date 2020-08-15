95ddf6cf5f149a3532df90f3f9f38abf04a663c2
beknobloch <bknobs4@icloud.com>
2020-08-14 22:45:33 -0400

    Beginning of chat functions

--- public/js/chat.js
+++ public/js/chat.js
@@ -25,9 +25,101 @@ function runChat (user)
 
             })
         })
-        
+        console.log(thisUser.name);
+        thisUser.convos.forEach( function(convo){
+            
+            console.log(convo);
+            let conversationList = document.getElementById("conversation-list");
+
+            let pfp = document.createElement("img");
+            pfp.src = convo.pfp;                        // Maybe change later, idk
+            
+            let titleText = document.createElement("div");
+            titleText.classList.add("title-text");
+            titleText.innerHTML = convo.name;
+
+            let latestDate = document.createElement("div");
+            latestDate.classList.add("latest-date");
+            latestDate.innerHTML = convo.date;
+
+            let latestMessage = document.createElement("div");
+            latestMessage.classList.add("conversation-message");
+            latestMessage.innerHTML = convo.latestMessage;
+
+            let conversationElement = document.createElement("div");
+            conversationElement.appendChild(pfp);
+            conversationElement.appendChild(titleText);
+            conversationElement.appendChild(latestDate);
+            conversationElement.appendChild(latestMessage);
+
+            conversationElement.addEventListener("click", function () {
+                displayConversation(convo.number);
+            })
+
+            conversationList.appendChild(conversationElement);
+
+        }
+        )
+
+        function displayConversation(convoNumber){
+            let thisConvo = thisUser.convos[convoNumber];
+            thisConvo.messages.forEach( function(message) {
+
+                let chatMessageList = document.getElementById("chat-message-list");
+
+                let messageRow = document.createElement("div");
+                messageRow.classList.add("message-row")
+                
+                let messageContent = document.createElement("div");
+                messageContent.classList.add("message-content");
+
+                
+                if(message[0] == 0){
+                    // If you sent it
+                    messageRow.classList.add("you-message");
+                }
+                else if(message[0] == 1){
+                    // If the other person sent it
+                    messageRow.classList.add("other-message");
+                    let messageImage = document.createElement("img");
+                    messageImage.src = thisConvo.pfp;
+                    messageImage.height = "40px";
+                    messageImage.width = "40px";
+                    messageContent.appendChild(messageImage);
+                }
+
+                let messageText = document.createElement("div");
+                messageText.classList.add("message-text");
+                messageText.innerHTML = message[2];
+
+                let messageTime = document.createElement("div");
+                messageTime.classList.add("message-time");
+                messageTime.innerHTML = message[1];
+
+                messageContent.appendChild(messageText);
+                messageContent.appendChild(messageTime);
+
+                messageRow.appendChild(messageContent);
+
+                chatMessageList.append(messageRow);
+            }
+            )
+        }
+
     }
     else {
         console.log('you\'re not logged in!')
     }
+}
+
+document.getElementById("send-box")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        sendMessage();
+    }
+});
+
+function sendMessage(){
+    
 }
